9d12de44e9e49fcc7671a049a9e4a10713533efd
Shoro749 <romanbro749@gmail.com>
2024-03-23 19:22:59 +0200

    Add project files.

---
+++ Pages/HomeScreen.xaml.cs
@@ -0,0 +1,46 @@
+﻿using ExamTask.Navigator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExamTask.Pages
+{
+    /// <summary>
+    /// Interaction logic for HomeScreen.xaml
+    /// </summary>
+    public partial class HomeScreen : UserControl
+    {
+        public HomeScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void InfoClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Login: Abmin\nPassword: 1234");
+        }
+
+        private void Enter_Click(object sender, RoutedEventArgs e)
+        {
+            if (tb_login.Text == "Admin" && tb_password.Text == "1234")
+            {
+                NavigatorObject.Switch(new Bookstore());
+            }
+            else
+            {
+                //NavigatorObject.Switch(new ErrorScreen());
+            }
+        }
+    }
+}
