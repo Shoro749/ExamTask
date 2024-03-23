41854ed31db561335632105a2d5be40ec0d9a8c3
Shoro749 <romanbro749@gmail.com>
2024-03-23 19:36:18 +0200

    Added models

---
+++ Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamTask.Models
+{
+    public class Book
+    {
+        public string Title { get; set; } 
+        public string Author { get; set; } 
+        public string Publisher { get; set; } 
+        public int PageCount { get; set; } 
+        public string Genre { get; set; } 
+        public int Year { get; set; } 
+        public decimal CostPrice { get; set; } 
+        public decimal SellingPrice { get; set; } 
+        public bool IsContinuation { get; set; } 
+    }
+}
