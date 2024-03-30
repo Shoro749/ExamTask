cc1b5aaed9ce904aa30e98636df9d51ae9525715
Shoro749 <romanbro749@gmail.com>
2024-03-30 18:35:05 +0200

    Added SellScreen

--- Pages/Bookstore.xaml.cs
+++ Pages/Bookstore.xaml.cs
@@ -28,6 +28,36 @@ namespace ExamTask.Pages
         {
             InitializeComponent();
             _dataContext = dataContext;
+
+            //Book book1 = new Book
+            //{
+            //    Title = "The Great Gatsby",
+            //    Author = "F. Scott Fitzgerald",
+            //    Publisher = "Scribner",
+            //    PageCount = 180,
+            //    Genre = "Classic",
+            //    Year = 1925,
+            //    CostPrice = 10,
+            //    SellingPrice = 20,
+            //    IsContinuation = false,
+            //};
+
+            //Book book2 = new Book
+            //{
+            //    Title = "Harry Potter and the Philosopher's Stone",
+            //    Author = "J.K. Rowling",
+            //    Publisher = "Bloomsbury Publishing",
+            //    PageCount = 223,
+            //    Genre = "Fantasy",
+            //    Year = 1997,
+            //    CostPrice = 8,
+            //    SellingPrice = 15,
+            //    IsContinuation = true,
+            //};
+            //dataContext.Book.Add(book1);
+            //dataContext.Book.Add(book2);
+            //_dataContext.SaveChanges();
+
             dataGrid.ItemsSource = _dataContext.Book.ToList();
         }
 
@@ -102,8 +132,24 @@ namespace ExamTask.Pages
 
         private void SellBook_Click(object sender, RoutedEventArgs e)
         {
-            //not finished
-            NavigatorObject.Switch(new SellBookScreen());
+            var selectedItems = dataGrid.SelectedItems;
+            if (selectedItems.Count > 0)
+            {
+                List<Book> selectedBooks = new List<Book>();
+                foreach (var item in selectedItems)
+                {
+                    if (item is Book book)
+                    {
+                        selectedBooks.Add(book);
+                    }
+                }
+                NavigatorObject.Switch(new SellBookScreen(_dataContext, selectedBooks));
+                dataGrid.ItemsSource = _dataContext.Book.ToList();
+            }
+            else
+            {
+                MessageBox.Show("Please select at least one item to edit.");
+            }
         }
 
         private void WriteOfBook_Click(object sender, RoutedEventArgs e)
--- Pages/SellBookScreen.xaml.cs
+++ Pages/SellBookScreen.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using ExamTask.Models;
+using ExamTask.Navigator;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,10 +22,63 @@ namespace ExamTask.Pages
     /// </summary>
     public partial class SellBookScreen : UserControl
     {
+        private readonly DataContext _dataContext;
+        private List<Book> _books;
         public SellBookScreen(DataContext dataContext, List<Book> books)
         {
             InitializeComponent();
+
+            _dataContext = dataContext;
             tb_discount.Text = "0";
+            _books = books;
+
+            int sum = 0;
+            foreach (Book book in books)
+            {
+                string str = $"Book: {book.Title}; Price: {book.SellingPrice};";
+                sum += Convert.ToInt32(book.SellingPrice);
+                lb_bill.Items.Add(str);
+            }
+            l_value.Content = $"VALUE: {sum}";
+            l_totalValue.Content = $"TOTAL VALUE: {sum}";
+
+            tb_discount.TextChanged += (sender, e) =>
+            {
+                if (int.TryParse(tb_discount.Text, out int discount))
+                {
+                    int totalValue = Convert.ToInt32(discount);
+
+                    l_totalValue.Content = "TOTAL VALUE: " + (sum - totalValue).ToString();
+                }
+                else
+                {
+                    throw new ArgumentException(nameof(tb_discount.Text));
+                }
+            };
+        }
+
+        private void SellBook_Click(object sender, RoutedEventArgs e)
+        {
+            List<int> selectedIds = new List<int>();
+
+            foreach (var selectedItem in _books)
+            {
+                if (selectedItem is Book yourObject)
+                {
+                    int id = yourObject.Id;
+                    selectedIds.Add(id);
+                }
+            }
+
+            foreach (var id in selectedIds)
+            {
+                Book item = _dataContext.Set<Book>().Find(id) ??
+                throw new ArgumentNullException();
+
+                _dataContext.Set<Book>().Remove(item);
+                _dataContext.SaveChanges();
+            }
+            NavigatorObject.Switch(new Bookstore(_dataContext));
         }
     }
 }
